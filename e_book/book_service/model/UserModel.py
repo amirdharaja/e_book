3c865b5a92e0d1d6d852a1833699fadda7ef04da
Amirdharajan M <amirtharajan1990@gmail.com>
2020-04-11 13:22:09 +0530

    Completed

--- e_book/book_service/model/UserModel.py
+++ e_book/book_service/model/UserModel.py
@@ -13,7 +13,7 @@ class User(models.Model):
     email               =    models.CharField(max_length=255, null=False)
     permanent_address   =    models.CharField(max_length=255, null=True)
     temporary_address   =    models.CharField(max_length=255, null=False)
-    pincode             =    models.CharField(max_length=255, null=False)
+    pincode             =    models.CharField(max_length=16, null=False)
     created_at          =    models.DateTimeField(auto_now_add=True)
     updated_at          =    models.DateTimeField(null=True)
 
--- e_book/book_service/model/__init__.py
+++ e_book/book_service/model/__init__.py
@@ -1,7 +1,6 @@
 from .BookModel import Book
 from .CartModel import Cart
 from .FavoriteBookModel import FavoriteBook
-from .UserBookModel import UserBook
 from .UserModel import User
 from .TokenModel import Token
 from .LoginHistoryModel import LoginHistory
